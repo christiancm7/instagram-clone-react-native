28ec67cef932e556f14658707e7b8635d1ff71a2
Christian <atticuschris@yahoo.com>
2020-11-30 07:23:06 -0600

    new update to app

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import AppNavigator from './src/AppNavigator';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
+// 
 function App() {
   return <NavigationContainer>{<AppNavigator />}</NavigationContainer>;
 }
